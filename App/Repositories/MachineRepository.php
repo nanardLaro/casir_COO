8203677c8f06c38b8cd842243db9282dfc320794
Leonard <leonard.larochette@etu.univ-grenoble-alpes.fr>
2023-04-03 08:08:54 +0200

    Modifs diverses

--- App/Repositories/MachineRepository.php
+++ App/Repositories/MachineRepository.php
@@ -2,7 +2,22 @@
 
 namespace App\Repositories;
 
+use App\Core\Tools;
+use App\Models\AbstractProduct;
+
 class MachineRepository extends AbstractRepository
 {
+
+    public function createProduct(int $idProduct) : AbstractProduct{
+        $fileProduct = file_get_contents('../Ressources/Product.txt');
+        $rows = explode('\n', $fileProduct);
+        arra_shift($rows);
+        if (sizeof($rows) >= $idProduct){
+            for($i = 0; $i < $idProduct; $i++){
+
+            }
+        }
+
+    }
 
 }
--- App/Services/MessageService.php
+++ App/Services/MessageService.php
@@ -8,17 +8,23 @@ use App\Repositories\MessageRepository;
 class MessageService {
 
     private function repository() : MessageRepository {
+
         return new MessageRepository();
+
     }
 
     public function parse(Caller $caller) {
+
         return $this->repository()->createFromCaller($caller);
+
     }
 
     public function persistAll(array $messages) {
+
         foreach($messages as $message) {
             $this->repository()->save($message);
         }
+
     }
 
 }
